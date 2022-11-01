071bd5da0768f84ba6d3f58ef379f5f9f87e844b
nevertheless0404 <nevertheless.hi@gmail.com>
2022-11-01 17:17:17 +0900

    fix : pjt.urls
    프로젝트 urls 수정

--- a_pjt/urls.py
+++ a_pjt/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("restaurants/", include("restaurants.urls")),
+    path("reviews/", include("reviews.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
