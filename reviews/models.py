81b6a63a4d2ac29ded1f50a24105c8d1d512b4e0
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-02 09:50:54 +0900

    feat : test
    - 테스트 완료

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail
 
 # Create your models here.
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="like_reviews")
+    image = ProcessedImageField(
+        blank=True,
+        processors=[Thumbnail(200, 300)],
+        format="JPEG",
+        options={"quality": 90},
+    )
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    content = models.TextField(max_length=150)
