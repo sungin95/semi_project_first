2c1ea3d2419970be6208d7c7e39f860d43cdd8ff
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-02 10:13:30 +0900

    feat : res create
    - res main 페이지에 음식점정보 등록 url 추가
    - res views.py create 함수 작성
    - res forms.html 생성

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('detail/<int:restaurant_pk>', views.detail, name='detail'),
     path('menu/', views.menu, name='menu'),
+    path('create/', views.create, name='create'),
 ]
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_http_methods, require_POST
 from .models import Restaurants
+from .forms import RestaurantForm
+from django.contrib import messages
 
 # Create your views here.
 def main(request):
@@ -10,7 +13,23 @@ def detail(request, restaurant_pk):
     context = {
         'restaurant': restaurant,
     }
-    return render(request, 'restaurants/detail.html')
+    return render(request, 'restaurants/detail.html', context)
 
 def menu(request):
     return render(request, 'restaurants/menu.html')
+
+@require_http_methods(['GET','POST'])
+def create(request):
+    if request.method == 'POST':
+        form = RestaurantForm(request.POST, files=request.FILES)
+        if form.is_valid():
+            restaurant = form.save(commit=False)
+            restaurant.user = request.user
+            restaurant.save()
+            messages.success(request, '글 작성이 완료되었습니다.')
+            return redirect('restaurants:main')
+    form = RestaurantForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'restaurants/forms.html', context)
