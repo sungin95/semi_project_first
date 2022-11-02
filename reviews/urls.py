f4b5cda019e260625d9d13616f6bc2d2e0ea40a2
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2022-11-02 11:30:09 +0900

    feat : url
    url 주석 처리

--- reviews/urls.py
+++ reviews/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path("<int:review_pk>/update", views.update, name="update"),
     path("<int:review_pk>/delete", views.update, name="delete"),
     path("<int:review_pk>/like", views.like, name="like"),
-    path("<int:review_pk>/comments", views.comments, name="comments"),
-    path(
-        "<int:reviews_pk>/comments/<int:comment_pk>/update",
-        views.comment_update,
-        name="comment_update",
-    ),
-    path(
-        "<int:reviews_pk>/comments/<int:comment_pk>/delete",
-        views.comment_delete,
-        name="comment_delete",
-    ),
+    # path("<int:review_pk>/comments", views.comments, name="comments"),
+    # path(
+    #     "<int:reviews_pk>/comments/<int:comment_pk>/update",
+    #     views.comment_update,
+    #     name="comment_update",
+    # ),
+    # path(
+    #     "<int:reviews_pk>/comments/<int:comment_pk>/delete",
+    #     views.comment_delete,
+    #     name="comment_delete",
+    # ),
 ]
