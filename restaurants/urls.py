39b499caa097306420bc87e41eaf919001b91808
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-02 10:51:27 +0900

    res url bug fix

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('menu/', views.menu, name='menu'),
     path('create/', views.create, name='create'),
-    path('/<int:restaurant_pk>detail/', views.detail, name='detail'),
-    path('/<int:restaurant_pk>update/', views.update, name='update'),
-    path('/<int:restaurant_pk>delete/', views.delete, name='delete'),
+    path('/<int:restaurant_pk>/detail/', views.detail, name='detail'),
+    path('/<int:restaurant_pk>/update/', views.update, name='update'),
+    path('/<int:restaurant_pk>/delete/', views.delete, name='delete'),
 ]
